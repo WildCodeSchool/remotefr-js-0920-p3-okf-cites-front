fba10ec231cdb8489f67375d6e9bdac9fe9ab878
Max <maxime.delannoy@outlook.com>
2021-01-26 10:49:22 +0100

    data added

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { RadialAreaChart, RadialAxis, RadialAreaSeries } from 'reaviz';
+import {
+  RadialAreaChart,
+  RadialAxis,
+  RadialAreaSeries,
+  PieChart,
+} from 'reaviz';
 import styles from './Data.module.css';
 
 export default function Data() {
@@ -8,7 +13,8 @@ export default function Data() {
   const [datavis, setDatavis] = useState({
     kingdomDataWikiId: '',
     kingdomDataCites: '',
-    kingdomDataCommon: '',
+    kingdomDataCommonFr: '',
+    kingdomDataCommonEn: '',
     kingdomDataImage: '',
     kingdomDataTotal: '',
     kingdomDataArticle: '',
@@ -58,92 +64,131 @@ export default function Data() {
   console.log(datavis);
 
   return (
-    <div className={styles.data}>
+    <div className={styles.global}>
       {datavis.kingdomDataWikiId[0] === undefined ? (
         ''
       ) : (
-        <div>
-          <h2 className={styles.titre}>
-            Informations manquantes parmi les animaux
-          </h2>
-          <RadialAreaChart
-            height={450}
-            width={450}
+        <div className={styles.pie}>
+          <h2 className={styles.titre}>Répartition des espèces</h2>
+          <PieChart
+            height={300}
+            width={300}
             data={[
+              { key: 'Animal', data: datavis.kingdomDataTotal[0].count },
               {
-                key: 'ID Wikidata',
-                data: datavis.kingdomDataWikiId[0].count,
+                key: 'Végétal',
+                data: datavis.kingdomDataTotal[1].count,
+                color: 'c',
               },
-              { key: 'CITES', data: datavis.kingdomDataCites[0].count },
-              { key: 'Photo', data: datavis.kingdomDataImage[0].count },
-              { key: 'Nom commun', data: datavis.kingdomDataCommon[0].count },
-              { key: 'Description', data: datavis.kingdomDataArticle[0].count },
             ]}
-            innerRadius={innerRadius}
-            axis={<RadialAxis type="category" />}
-            series={
-              <RadialAreaSeries
-                colorScheme={color}
-                animated={animated}
-                interpolation={interpolation}
-                // area={<RadialArea gradient={gradient} />}
-              />
-            }
-            // axis={
-            //   <RadialAxis
-            //     arcs={<RadialAxisArcSeries count={arcCount} />}
-            //     ticks={
-            //       <RadialAxisTickSeries
-            //         count={tickCount}
-            //         tick={
-            //           <RadialAxisTick
-            //             line={<RadialAxisTickLine position={tickPosition} />}
-            //             label={<RadialAxisTickLabel autoRotate={autoRotate} />}
-            //           />
-            //         }
-            //       />
-            //     }
-            //   />
-            // }
           />
         </div>
       )}
 
-      {datavis.kingdomDataWikiId[0] === undefined ? (
-        ''
-      ) : (
-        <div>
-          <h2 className={styles.titre}>
-            Informations manquantes parmi les plantes
-          </h2>
-          <RadialAreaChart
-            height={450}
-            width={450}
-            data={[
-              {
-                key: 'ID Wikidata',
-                data: datavis.kingdomDataWikiId[1].count,
-              },
-              { key: 'CITES', data: datavis.kingdomDataCites[1].count },
-              { key: 'Photo', data: datavis.kingdomDataImage[1].count },
-              {
-                key: 'Nom commun',
-                data: datavis.kingdomDataCommon[1].count,
-              },
-              { key: 'Description', data: datavis.kingdomDataArticle[0].count },
-            ]}
-            innerRadius={innerRadius}
-            axis={<RadialAxis type="category" />}
-            series={
-              <RadialAreaSeries
-                colorScheme={color}
-                animated={animated}
-                interpolation={interpolation}
-              />
-            }
-          />
-        </div>
-      )}
+      <div className={styles.data}>
+        {datavis.kingdomDataWikiId[0] === undefined ? (
+          ''
+        ) : (
+          <div>
+            <h2 className={styles.titre}>
+              Informations manquantes parmi les animaux
+            </h2>
+            <RadialAreaChart
+              height={450}
+              width={450}
+              data={[
+                {
+                  key: 'ID Wikidata',
+                  data: datavis.kingdomDataWikiId[0].count,
+                },
+                { key: 'CITES', data: datavis.kingdomDataCites[0].count },
+                { key: 'Photo', data: datavis.kingdomDataImage[0].count },
+                {
+                  key: 'Nom commun fr',
+                  data: datavis.kingdomDataCommonFr[0].count,
+                },
+                {
+                  key: 'Description',
+                  data: datavis.kingdomDataArticle[0].count,
+                },
+                {
+                  key: 'Nom commun En',
+                  data: datavis.kingdomDataCommonEn[0].count,
+                },
+              ]}
+              innerRadius={innerRadius}
+              axis={<RadialAxis type="category" />}
+              series={
+                <RadialAreaSeries
+                  colorScheme={color}
+                  animated={animated}
+                  interpolation={interpolation}
+                  // area={<RadialArea gradient={gradient} />}
+                />
+              }
+              // axis={
+              //   <RadialAxis
+              //     arcs={<RadialAxisArcSeries count={arcCount} />}
+              //     ticks={
+              //       <RadialAxisTickSeries
+              //         count={tickCount}
+              //         tick={
+              //           <RadialAxisTick
+              //             line={<RadialAxisTickLine position={tickPosition} />}
+              //             label={<RadialAxisTickLabel autoRotate={autoRotate} />}
+              //           />
+              //         }
+              //       />
+              //     }
+              //   />
+              // }
+            />
+          </div>
+        )}
+
+        {datavis.kingdomDataWikiId[1] === undefined ? (
+          ''
+        ) : (
+          <div>
+            <h2 className={styles.titre}>
+              Informations manquantes parmi les plantes
+            </h2>
+            <RadialAreaChart
+              height={450}
+              width={450}
+              data={[
+                {
+                  key: 'ID Wikidata',
+                  data: datavis.kingdomDataWikiId[1].count,
+                },
+                { key: 'CITES', data: datavis.kingdomDataCites[1].count },
+                { key: 'Photo', data: datavis.kingdomDataImage[1].count },
+                {
+                  key: 'Nom commun fr',
+                  data: datavis.kingdomDataCommonFr[1].count,
+                },
+                {
+                  key: 'Description',
+                  data: datavis.kingdomDataArticle[1].count,
+                },
+                {
+                  key: 'Nom commun En',
+                  data: datavis.kingdomDataCommonEn[1].count,
+                },
+              ]}
+              innerRadius={innerRadius}
+              axis={<RadialAxis type="category" />}
+              series={
+                <RadialAreaSeries
+                  colorScheme={color}
+                  animated={animated}
+                  interpolation={interpolation}
+                />
+              }
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
